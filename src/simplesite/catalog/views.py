4e28a98a66e2c77ef2210169611b28ef94d54573
Ros <ros.litvin@gmail.com>
2021-02-10 16:53:51 +0300

    add about book page

--- src/simplesite/catalog/views.py
+++ src/simplesite/catalog/views.py
@@ -9,5 +9,14 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'num_books':num_books, 'books': books}
+        context = {'num_books':num_books, 'books': books}
+    )
+
+def about(request, pk):
+    about_book = Book.objects.get(pk=pk)
+
+    return render(
+        request,
+        'about_book.html',
+        context = {'about_book': about_book}
     )
