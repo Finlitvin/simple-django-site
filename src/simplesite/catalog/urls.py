ada4b42fabfc588ca950b45d25c9cb64bf4e0a57
Ros <ros.litvin@gmail.com>
2021-02-10 20:53:16 +0300

    add form to create

--- src/simplesite/catalog/urls.py
+++ src/simplesite/catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('books/<int:pk>', views.about, name='book-detail')
+    path('', views.IndexView.as_view(), name = 'index'),
+    path(r'^book/(?P<pk>\d+)$', views.BookAboutView.as_view(), name = 'book-detail'),
+    url('/add', views.BookCreate.as_view(), name = 'book_create'),
+    #path('/add', views.add_book, name = 'add_book')
 ]
--- src/simplesite/catalog/views.py
+++ src/simplesite/catalog/views.py
@@ -1,22 +1,58 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView
+#from .forms import AddBookForm
 from .models import Book
 
 
-def index(request):
-    num_books = Book.objects.all().count()
-    books = Book.objects.all()
+class IndexView(generic.ListView):
+    model = Book
+    context_object_name = 'books'
+    template_name = 'index.html'
 
-    return render(
-        request,
-        'index.html',
-        context = {'num_books':num_books, 'books': books}
-    )
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['num_books'] = Book.objects.all().count()
+        return context
 
-def about(request, pk):
-    about_book = Book.objects.get(pk=pk)
+class BookAboutView(generic.DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'about_book.html'
 
-    return render(
-        request,
-        'about_book.html',
-        context = {'about_book': about_book}
-    )
+class BookCreate(CreateView):
+    model = Book
+    template_name = 'book_form.html'
+    fields = '__all__'
+
+
+
+
+######## or ########
+#def add_book(request):
+#    form = AddBookForm()
+#
+#    return render(
+#        request,
+#        'add_book.html',
+#        {'form': form}
+#    )
+
+#def index(request):
+#    num_books = Book.objects.all().count()
+#    books = Book.objects.all()
+#
+#    return render(
+#        request,
+#        'index.html',
+#        context = {'num_books':num_books, 'books': books}
+#    )
+
+#def about(request, pk):
+#    about_book = Book.objects.get(pk=pk)
+#
+#    return render(
+#        request,
+#        'about_book.html',
+#        context = {'about_book': about_book}
+#    )
